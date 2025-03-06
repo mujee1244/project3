531e092cbed894dfb90b94b0f29a46c676c6d4e9
mujee1244 <mujeebur@gmail.com>
2025-03-06 12:54:19 +0500

    add js tools, made map responsive, fixed some mistakes, etc

--- script/main.js
+++ script/main.js
@@ -1,1 +1,9 @@
- 
+document.getElementById("darkModeToggle").addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+})
+document.getElementById("scrollToTop").addEventListener("click", () => {
+    window.scrollTo({ top: 0, behaviour: "smooth" });
+    console.log("click")
+})
+
+console.log("script runs")
